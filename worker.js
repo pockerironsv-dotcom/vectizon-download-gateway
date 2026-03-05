8afc8b704e3b5fef92aed58908a50c1795a957d3
pockerironsv-dotcom <pockerironsv@gmail.com>
2026-03-05 12:32:10 +0200

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,25 @@
+export default {
+  async fetch(request, env) {
+
+    const url = new URL(request.url)
+
+    const key = url.searchParams.get("file")
+
+    if (!key) {
+      return new Response("Missing file parameter", { status: 400 })
+    }
+
+    const object = await env.ASSETS.get(key)
+
+    if (!object) {
+      return new Response("File not found", { status: 404 })
+    }
+
+    return new Response(object.body, {
+      headers: {
+        "Content-Type": "application/octet-stream",
+        "Content-Disposition": `attachment; filename="${key.split("/").pop()}"`
+      }
+    })
+  }
+}
